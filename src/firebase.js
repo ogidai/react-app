42af3b5e6f057dc5442c73fc4e2419f7b200d61f
荻久保大輝 <ogidai610@gmail.com>
2020-12-30 18:00:45 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
 
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_DATABASE_URL,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCHLV807zJgI18zDBrywIPm_pJHoqEv7GA",
-    authDomain: "chat-with-ogi.firebaseapp.com",
-    databaseURL: "https://chat-with-ogi-default-rtdb.firebaseio.com/",
-    projectId: "chat-with-ogi",
-    storageBucket: "chat-with-ogi.appspot.com",
-    messagingSenderId: "492194568180",
-    appId: "1:492194568180:web:722dd0dfdef5c88a2d0ba1"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
   };
 
 firebase.initializeApp(firebaseConfig);
