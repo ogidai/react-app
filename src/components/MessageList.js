a7f900ea6ff56c11a7df81affba2be38a975f080
荻久保大輝 <ogidai610@gmail.com>
2020-12-30 23:47:07 +0900

    create fetch messages function

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import {makeStyles} from '@material-ui/core/styles';
+
+import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles({
     root:{
@@ -8,7 +10,28 @@ const useStyles = makeStyles({
 });
 
 const MessageList = () => {
+    const [messages, setMessages] = useState([]);
     const classes = useStyles();
+
+    useEffect(() => {
+        messagesRef
+        .orderByKey()
+        .limitToLast(3)
+        .on('value', (snapshot) => {
+            const messsages = snapshot.val();
+            if (messages === null) return;
+            const entries = Object.entries(messsages);
+            const newMessages = entries.map((entry) => {
+                // １行でまとめる
+                const [key, nameAndText] = entry;
+                // const key = entry[0];
+                // const nameAndText = entry[1];
+                return { key, ...nameAndText };
+            });
+            console.log(newMessages);
+            setMessages(newMessages);
+        });
+    }, []);
     return <div className={classes.root}>MessageList</div>;
 };
 
